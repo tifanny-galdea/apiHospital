9f34ccb7fd648cfd4fb0f8e84bf9fdc13d458728
tifanny-galdea <tifannygm13@gmail.com>
2025-05-19 23:04:18 -0500

    Cambio 1

--- src/controladores/autenticacion_ctrl.js
+++ src/controladores/autenticacion_ctrl.js
@@ -1,7 +1,4 @@
 import { conmysql } from "../bd.js";
-import jwt from "jsonwebtoken";
-import md5 from "md5";
-import { JWT_CLAVE } from "../config.js";
 
 export const login = async (req, res) => {
   const { usr_usuario, usr_clave } = req.body;
@@ -9,31 +6,29 @@ export const login = async (req, res) => {
     return res.status(400).json({ message: 'Faltan credenciales' });
   }
   try {
-    const claveMd5 = md5(usr_clave);
-    const [rows] = await conmysql.query(
-      'SELECT * FROM usuarios WHERE usr_usuario = ? AND usr_clave = ? AND usr_activo = 1',
-      [usr_usuario, claveMd5]
+    //Obtener Usuario
+    const [user] = await conmysql.query(
+      'SELECT u.id_usuario, u.usuario, ru.id_rol, r.descripcion AS rol FROM usuario u JOIN rol_usuario ru ON ru.id_usuario = u.id_usuario JOIN rol r ON r.id_rol = ru.id_rol WHERE u.usuario = ? AND u.password = ? AND ru.estado = 1',
+      [usr_usuario, usr_clave]
     );
-    if (rows.length === 0) {
+    if (user.length === 0) {
       return res.status(401).json({ message: 'Credenciales inválidas' });
     }
-    const usuario = rows[0];
-    const token = jwt.sign(
-      { id: usuario.usr_id, usuario: usuario.usr_usuario },
-      JWT_CLAVE,
-      { expiresIn: '8h' }
+    //Obtener menú de ese rol
+    const [menu] = await conmysql.query(
+      'SELECT m.descripcion, m.url, m.icono FROM rol_menu rm JOIN menu m ON m.id_opcion = rm.id_opcion WHERE rm.id_rol = ? AND rm.estado = 1', 
+      [user.id_rol]
     );
+
+    //Retornar datos del usuario + menú
     res.json({
-      message: 'Autenticación correcta',
-      token,
-      usuario: {
-        id: usuario.usr_id,
-        usuario: usuario.usr_usuario,
-        nombre: usuario.usr_nombre,
-        correo: usuario.usr_correo,
-      }
+        usuario: user.usuario,
+        rol: user.rol,
+        menu
     });
+    
   } catch (error) {
     res.status(500).json({ message: 'Error en el servidor', error: error.message });
   }
+
 };
